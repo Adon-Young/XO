0127728a318c1cf19a3f831fa09bcfec854157ef
ClockworkKnowledge <clockworkKnowledge.aj@gmail.com>
2025-02-26 10:42:45 +0000

    notes on roulette script

--- Assets/scirpts/new scripts/Roulette.cs
+++ Assets/scirpts/new scripts/Roulette.cs
@@ -1,106 +1,51 @@
-﻿//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-//public class Roulette : MonoBehaviour
-//{
-//    public GameObject rouletteScreen;  // The roulette UI panel
-//    public List<GameObject> skinButtons; // The UI buttons representing skins
-//    public Text resultText;            // Display the result (for debugging)
 
-//    private int preDeterminedSkin = -1; // The skin that will be unlocked
-//    private List<int> unlockedSkins = new List<int>();
+public class Roulette : MonoBehaviour
+{
+    public List<GameObject> skinLocks = new List<GameObject>();
+    //list of game object i.e. the locks on the skins that need to be removed...
+    //can add publically from 1->15
+    //disable lock ui over buttons
 
-//    void Start()
-//    {
-//        LoadUnlockedSkins();
-//        UpdateUI();
-//    }
+    /*
+     weights per item for each of the 15 items
 
-//    void LoadUnlockedSkins()
-//    {
-//        string unlockedData = PlayerPrefs.GetString("unlockedSkins", "");
-//        if (!string.IsNullOrEmpty(unlockedData))
-//        {
-//            unlockedSkins = new List<int>(System.Array.ConvertAll(unlockedData.Split(','), int.Parse));
-//        }
-//    }
+    1-
+    2-
+    3-
+    4-
+    5-
+    6-
+    7-
+    8-
+    9-
+    10-
+    11-
+    12-
+    13-
+    14-
+    15-
+    (16->already unlocked i.e. default)
+     */
 
-//    void SaveUnlockedSkins()
-//    {
-//        PlayerPrefs.SetString("unlockedSkins", string.Join(",", unlockedSkins));
-//        PlayerPrefs.Save();
-//    }
 
-//    public void SpendTokenAndOpenRoulette()
-//    {
-//        // Check if the player has enough tokens (assume TokenSystem is managing this)
-//        TokenSystem tokenSystem = FindObjectOfType<TokenSystem>();
-//        if (tokenSystem != null && tokenSystem.CanSpendToken())
-//        {
-//            tokenSystem.SpendTokens();
-//            preDeterminedSkin = DetermineSkin();  // Choose skin before opening roulette
-//            OpenRouletteScreen();
-//        }
-//        else
-//        {
-//            Debug.Log("Not enough tokens!");
-//        }
-//    }
+    //roulette button. this script is attached to the button that takes the player to te roulette wheel
+    public void RouletteButton()
+    {
+     
+      
 
-//    int DetermineSkin()
-//    {
-//        List<int> availableSkins = new List<int>();
+    }
 
-//        // Find locked skins
-//        for (int i = 0; i < skinButtons.Count; i++)
-//        {
-//            if (!unlockedSkins.Contains(i))
-//            {
-//                availableSkins.Add(i);
-//            }
-//        }
 
-//        if (availableSkins.Count > 0)
-//        {
-//            // Unlock a new skin
-//            int chosenSkin = availableSkins[Random.Range(0, availableSkins.Count)];
-//            unlockedSkins.Add(chosenSkin);
-//            SaveUnlockedSkins();
-//            return chosenSkin;
-//        }
-//        else
-//        {
-//            // All skins unlocked → give duplicate
-//            return Random.Range(0, skinButtons.Count);
-//        }
-//    }
 
-//    void OpenRouletteScreen()
-//    {
-//        rouletteScreen.SetActive(true);
-//        ShowResult();
-//        UpdateUI();
-//    }
 
-//    void ShowResult()
-//    {
-//        if (resultText != null)
-//        {
-//            string resultMessage = unlockedSkins.Contains(preDeterminedSkin)
-//                ? $"You unlocked: Skin {preDeterminedSkin}!"
-//                : "Duplicate Skin!";
-//            resultText.text = resultMessage;
-//        }
-//    }
 
-//    void UpdateUI()
-//    {
-//        for (int i = 0; i < skinButtons.Count; i++)
-//        {
-//            bool isUnlocked = unlockedSkins.Contains(i);
-//            skinButtons[i].GetComponent<Button>().interactable = isUnlocked;
-//            skinButtons[i].transform.Find("LockImage").gameObject.SetActive(!isUnlocked);
-//        }
-//    }
-//}
+
+
+}
