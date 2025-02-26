a3dbd0d13597a7f24b7c192d53b3d9e1f072556f
ClockworkKnowledge <clockworkKnowledge.aj@gmail.com>
2025-02-26 10:54:42 +0000

    slight update

--- Assets/scirpts/new scripts/Roulette.cs
+++ Assets/scirpts/new scripts/Roulette.cs
@@ -11,34 +11,47 @@ public class Roulette : MonoBehaviour
     //list of game object i.e. the locks on the skins that need to be removed...
     //can add publically from 1->15
     //disable lock ui over buttons
+    public int[] valuePerItem;
+    int randomValue = Random.Range(1,101);//random range between 1->100 or should it be 0->101
+    
 
     /*
      weights per item for each of the 15 items
-
     1-
     2-
     3-
     4-
+    ------------------------
     5-
     6-
     7-
     8-
+    ------------------------
     9-
     10-
     11-
     12-
+    ------------------------
     13-
     14-
     15-
     (16->already unlocked i.e. default)
      */
 
+    public void Start()
+    {
 
+    }
+    
+    
+    
+    
     //roulette button. this script is attached to the button that takes the player to te roulette wheel
     public void RouletteButton()
     {
-     
       
+
+
 
     }
 
